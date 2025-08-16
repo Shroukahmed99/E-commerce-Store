1ad4617fe64be1f5e2edc6796121ab32e287e6c1
Shrouk Ahmed <shroukkaoud7@gmail.com>
2025-08-16 13:26:11 +0200

    connectivity is done

--- lib/ecommerce_store_app.dart
+++ lib/ecommerce_store_app.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_store/core/app/connectivity_controller.dart';
 import 'package:ecommerce_store/core/app/env.variables.dart';
+import 'package:ecommerce_store/core/common/screens/no_network_screen.dart';
 import 'package:flutter/material.dart';
 
 class EcommerceStoreApp extends StatelessWidget {
@@ -6,12 +8,37 @@ class EcommerceStoreApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: EnvVariables.instace.debugShow ,
-home: Scaffold(
-  appBar: AppBar(title:const Text('Shopping App'),backgroundColor: Colors.deepPurple,),
-  body:const Center(child:  Text('Shopping')),
-),
+    return ValueListenableBuilder(
+      valueListenable: ConnectivityController.instance.isConnected,
+      builder: (_, value, _) {
+        if (value) {
+          return MaterialApp(
+            title: "Ecommerce Store",
+            debugShowCheckedModeBanner: EnvVariables.instace.debugShow,
+            // builder: (context, widget) {
+            //   ConnectivityController.instance.init();
+            //   return widget!;
+            // },
+           home: Scaffold(
+  appBar: AppBar(
+    title: const Text("Shopping"),
+    backgroundColor: Colors.blue,
+  ),
+  body: const Center(
+    child: Text('Welcome to Ecommerce Store!'),
+  ),
+)
+
+          );
+        } else {
+          return MaterialApp(
+            title: "No Network ",
+            debugShowCheckedModeBanner: EnvVariables.instace.debugShow,
+            home: const NoNetworkScreen(),
+          );
+        }
+        ;
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_store/core/app/connectivity_controller.dart';
 import 'package:ecommerce_store/core/app/env.variables.dart';
 import 'package:ecommerce_store/ecommerce_store_app.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,13 +10,13 @@ void main() async {
   await Firebase.initializeApp();
   
   await EnvVariables.instace.init(envType: EnvTypeEnum.dev);
+  await ConnectivityController.instance.init();
 
-  await SystemChrome.setPreferredOrientations([
-  DeviceOrientation.portraitUp,
-  DeviceOrientation.portraitDown,
-]);
-
-runApp(const EcommerceStoreApp());
+await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp],
+  ).then((_) {
+    runApp(const EcommerceStoreApp());
+  });
 
 
  
