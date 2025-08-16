b0079a2f1c02f02aff7f36a2a5d9e13d03b68363
Shrouk Ahmed <shroukkaoud7@gmail.com>
2025-08-16 16:42:22 +0200

    font family and weight  is done

--- lib/ecommerce_store_app.dart
+++ lib/ecommerce_store_app.dart
@@ -1,6 +1,8 @@
 import 'package:ecommerce_store/core/app/connectivity_controller.dart';
 import 'package:ecommerce_store/core/app/env.variables.dart';
 import 'package:ecommerce_store/core/common/screens/no_network_screen.dart';
+import 'package:ecommerce_store/core/style/images/fonts/font_family_helper.dart';
+import 'package:ecommerce_store/core/style/images/fonts/font_weight_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -9,40 +11,72 @@ class EcommerceStoreApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: ConnectivityController.instance.isConnected,
-      builder: (_, value, _) {
-        if (value) {
-          return ScreenUtilInit(
-            designSize: const Size(360, 690),
-            minTextAdapt: true,
-            child: MaterialApp(
-              title: "Ecommerce Store",
-              debugShowCheckedModeBanner: EnvVariables.instace.debugShow,
-              // builder: (context, widget) {
-              //   ConnectivityController.instance.init();
-              //   return widget!;
-              // },
-             home: Scaffold(
-              appBar: AppBar(
-                title: const Text("Shopping"),
-                backgroundColor: Colors.blue,
-              ),
-              body: const Center(
-                child: Text('Welcome to Ecommerce Store!'),
-              ),
-            )
-            
-            ),
-          );
-        } else {
-          return MaterialApp(
-            title: "No Network ",
-            debugShowCheckedModeBanner: EnvVariables.instace.debugShow,
-            home: const NoNetworkScreen(),
-          );
-        }
-        ;
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return ValueListenableBuilder(
+          valueListenable: ConnectivityController.instance.isConnected,
+          builder: (_, value, __) {
+            if (value) {
+              return MaterialApp(
+                title: "Ecommerce Store",
+                debugShowCheckedModeBanner: EnvVariables.instace.debugShow,
+                home: Scaffold(
+                  appBar: AppBar(
+                    title: const Text("Shopping"),
+                    backgroundColor: Colors.blue,
+                  ),
+                  body: Center(
+                    child: Column(
+                      children: [
+                        //old
+                        Text(
+                          'مرحبا بكم في متجرنا الالكتروني',
+                          style: TextStyle(fontSize: 25.sp ),
+                        ),
+                        Text(
+                          'Welcome to Ecommerce Store!',
+                          style: TextStyle(fontSize: 20.sp),
+                        ),
+// new
+
+Text(
+                          'مرحبا بكم في متجرنا الالكتروني',
+                          style: TextStyle(fontSize: 25.sp ,
+                          fontFamily: FontFamilyHelper.cairoArabic ,
+                          fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Welcome to Ecommerce Store!',
+                          style: TextStyle(fontSize: 20.sp,fontFamily: FontFamilyHelper.poppinsEnglish ,
+                          fontWeight: FontWeightHelper.regular),
+                          ),
+                          Text(
+                          'Welcome to Ecommerce Store!',
+                          style: TextStyle(fontSize: 20.sp,fontFamily: FontFamilyHelper.poppinsEnglish ,
+                          fontWeight: FontWeightHelper.medium),
+                          ),
+                      
+
+
+
+
+
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            } else {
+              return MaterialApp(
+                title: "No Network",
+                debugShowCheckedModeBanner: EnvVariables.instace.debugShow,
+                home: const NoNetworkScreen(),
+              );
+            }
+          },
+        );
       },
     );
   }
