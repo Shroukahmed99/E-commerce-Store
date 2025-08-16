1b3768d9246a26130ebbd30f2eedc96f3bc0939c
Shrouk Ahmed <shroukkaoud7@gmail.com>
2025-08-16 15:05:56 +0200

    ScreenUtilInit is done

--- lib/ecommerce_store_app.dart
+++ lib/ecommerce_store_app.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce_store/core/app/connectivity_controller.dart';
 import 'package:ecommerce_store/core/app/env.variables.dart';
 import 'package:ecommerce_store/core/common/screens/no_network_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EcommerceStoreApp extends StatelessWidget {
   const EcommerceStoreApp({super.key});
@@ -12,23 +13,27 @@ class EcommerceStoreApp extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (_, value, _) {
         if (value) {
-          return MaterialApp(
-            title: "Ecommerce Store",
-            debugShowCheckedModeBanner: EnvVariables.instace.debugShow,
-            // builder: (context, widget) {
-            //   ConnectivityController.instance.init();
-            //   return widget!;
-            // },
-           home: Scaffold(
-  appBar: AppBar(
-    title: const Text("Shopping"),
-    backgroundColor: Colors.blue,
-  ),
-  body: const Center(
-    child: Text('Welcome to Ecommerce Store!'),
-  ),
-)
-
+          return ScreenUtilInit(
+            designSize: const Size(360, 690),
+            minTextAdapt: true,
+            child: MaterialApp(
+              title: "Ecommerce Store",
+              debugShowCheckedModeBanner: EnvVariables.instace.debugShow,
+              // builder: (context, widget) {
+              //   ConnectivityController.instance.init();
+              //   return widget!;
+              // },
+             home: Scaffold(
+              appBar: AppBar(
+                title: const Text("Shopping"),
+                backgroundColor: Colors.blue,
+              ),
+              body: const Center(
+                child: Text('Welcome to Ecommerce Store!'),
+              ),
+            )
+            
+            ),
           );
         } else {
           return MaterialApp(
